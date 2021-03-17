ddc4913484625256c4f79ef7d82bc6cae08dc6f8
Vinicyus <vinicyus346@gmail.com>
2021-03-17 18:27:41 -0300

    Inicio de documentaçao com o swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,6 +30,7 @@ export default class ImportCategoryUseCase {
           });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on('error', (err) => {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import router from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
-app.listen(3333, () => console.log('Server started at port 3333.'));
+app.listen(3333, () => {
+  console.log('Server started at port 3333. ');
+  console.log('Server documentation on http://localhost:3333/api-docs');
+});
