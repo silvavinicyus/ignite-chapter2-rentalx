337c5192f4c3d47dcb43754fcfa92cf9dddb8fff
Vinicyus <vinicyus346@gmail.com>
2021-04-19 12:25:28 -0300

    fixing errors on server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/infra/typeorm/database';
+import '@shared/infra/typeorm';
 import '@shared/container';
 
 import { AppError } from '@shared/errors/AppError';
